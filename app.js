703cd667e3e864c3a28fd441fa0b7f83f06fa35a
CHKTAYS <catred.11@yandex.ru>
2024-04-27 17:10:56 +0300

    Добавил всё остальное

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const path = require('path')
-const { cors } = require('./middlewares/cors')
+const { cors }  = require('./middlewares/cors')
 const connectToDatabase = require('./database/connect')
-//const usersRouter = require()
-//const gamesRouter = require()
-//const categoriesRouter = require()
+const usersRouter = require('./routes/users')
+const gamesRouter = require('./routes/games')
+const categoriesRouter = require('./routes/categories')
 const PORT = 3000
 connectToDatabase()
-app.use(cors, bodyParser.json(), express.static(path.join(__dirname, 'public')))
+app.use(
+    cors,
+    bodyParser.json(),
+    express.static(path.join(__dirname, 'public')),
+    usersRouter,
+    gamesRouter,
+    categoriesRouter
+)
 
 app.listen(PORT, () => {
     console.log(`Запущен на http://localhost:${PORT}`)
