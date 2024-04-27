35ca3f951baccf026a6ec764f32ab0a2a6112e57
CHKTAYS <catred.11@yandex.ru>
2024-04-27 15:00:23 +0300

    Подключил ДБ

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const path = require('path')
+const { cors } = require('./middlewares/cors')
+const connectToDatabase = require('./database/connect')
+//const usersRouter = require()
+//const gamesRouter = require()
+//const categoriesRouter = require()
 const PORT = 3000
-
-app.use(express.static(path.join(__dirname, 'public')))
+connectToDatabase()
+app.use(cors, bodyParser.json(), express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => {
     console.log(`Запущен на http://localhost:${PORT}`)
